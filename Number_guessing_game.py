06851e0f5e14ac5c80fb45699cf9956bef883899
imadkhattak <imadkhattak99@gmail.com>
2025-08-20 20:24:39 +0500

    Number guessing game complete

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -1,6 +1,7 @@
 import random
 
 top_of_range = input("Enter the top of the range: ")
+guesses = 0
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
@@ -14,4 +15,25 @@ else:
 random_number = random.randint(0, top_of_range)
 
 while True:
-    
+    guesses += 1
+    user_guess = input(f"Guess a number: ")
+
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please enter a number next time.")
+        continue          
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+
+    elif user_guess > random_number:
+        print("You are above the number")
+        
+    else:
+        print("You are below the number")
+       
+
+print(f"You got it in {guesses} guesses!")
+print("Thanks for playing!")
